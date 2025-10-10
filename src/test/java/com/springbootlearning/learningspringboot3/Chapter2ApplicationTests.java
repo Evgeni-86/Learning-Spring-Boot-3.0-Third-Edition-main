28055fce105987b4aa889418434f38da9cff8b6b
Batanov Evgenii <batanovevgeni@mail.ru>
2025-10-10 09:39:56 +0300

    Chapter 2 (api)

--- src/test/java/com/springbootlearning/learningspringboot3/Chapter2ApplicationTests.java
+++ src/test/java/com/springbootlearning/learningspringboot3/Chapter2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springbootlearning.learningspringboot3.ch2;
+package com.springbootlearning.learningspringboot3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
