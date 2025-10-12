c64ac48baabe4860f6c0603c995c2c54a2919950
Batanov EVgenii <batanovevgeni@mail.ru>
2025-10-12 11:05:08 +0300

    Chapter 4 (security (csrf disable))

---
+++ src/main/java/com/springbootlearning/learningspringboot3/UserManagementRepository.java
@@ -0,0 +1,6 @@
+package com.springbootlearning.learningspringboot3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserManagementRepository extends JpaRepository<UserAccount, Long> {
+}
